3c2450f285122763e04195329d42c337228807ed
Javier Quintana ABK <Javier.Quintana@activision.com>
2022-08-22 00:25:05 -0700

    Unreal Engine 5 - Door Rotation and Triggers - Added Door as an actor using Blueprint

---
+++ Intermediate/Build/Win64/UnrealEditor/Inc/Week5/InteractableDoor.generated.h
@@ -0,0 +1,90 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef WEEK5_InteractableDoor_generated_h
+#error "InteractableDoor.generated.h already included, missing '#pragma once' in InteractableDoor.h"
+#endif
+#define WEEK5_InteractableDoor_generated_h
+
+#define FID_Week5_Source_Week5_Public_InteractableDoor_h_13_SPARSE_DATA
+#define FID_Week5_Source_Week5_Public_InteractableDoor_h_13_RPC_WRAPPERS
+#define FID_Week5_Source_Week5_Public_InteractableDoor_h_13_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_Week5_Source_Week5_Public_InteractableDoor_h_13_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAInteractableDoor(); \
+	friend struct Z_Construct_UClass_AInteractableDoor_Statics; \
+public: \
+	DECLARE_CLASS(AInteractableDoor, AStaticMeshActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Week5"), NO_API) \
+	DECLARE_SERIALIZER(AInteractableDoor)
+
+
+#define FID_Week5_Source_Week5_Public_InteractableDoor_h_13_INCLASS \
+private: \
+	static void StaticRegisterNativesAInteractableDoor(); \
+	friend struct Z_Construct_UClass_AInteractableDoor_Statics; \
+public: \
+	DECLARE_CLASS(AInteractableDoor, AStaticMeshActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Week5"), NO_API) \
+	DECLARE_SERIALIZER(AInteractableDoor)
+
+
+#define FID_Week5_Source_Week5_Public_InteractableDoor_h_13_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AInteractableDoor(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AInteractableDoor) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AInteractableDoor); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AInteractableDoor); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AInteractableDoor(AInteractableDoor&&); \
+	NO_API AInteractableDoor(const AInteractableDoor&); \
+public:
+
+
+#define FID_Week5_Source_Week5_Public_InteractableDoor_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AInteractableDoor(AInteractableDoor&&); \
+	NO_API AInteractableDoor(const AInteractableDoor&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AInteractableDoor); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AInteractableDoor); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AInteractableDoor)
+
+
+#define FID_Week5_Source_Week5_Public_InteractableDoor_h_10_PROLOG
+#define FID_Week5_Source_Week5_Public_InteractableDoor_h_13_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Week5_Source_Week5_Public_InteractableDoor_h_13_SPARSE_DATA \
+	FID_Week5_Source_Week5_Public_InteractableDoor_h_13_RPC_WRAPPERS \
+	FID_Week5_Source_Week5_Public_InteractableDoor_h_13_INCLASS \
+	FID_Week5_Source_Week5_Public_InteractableDoor_h_13_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_Week5_Source_Week5_Public_InteractableDoor_h_13_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Week5_Source_Week5_Public_InteractableDoor_h_13_SPARSE_DATA \
+	FID_Week5_Source_Week5_Public_InteractableDoor_h_13_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Week5_Source_Week5_Public_InteractableDoor_h_13_INCLASS_NO_PURE_DECLS \
+	FID_Week5_Source_Week5_Public_InteractableDoor_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> WEEK5_API UClass* StaticClass<class AInteractableDoor>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Week5_Source_Week5_Public_InteractableDoor_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
--- Intermediate/Build/Win64/UnrealEditor/Inc/Week5/Week5.init.gen.cpp
+++ Intermediate/Build/Win64/UnrealEditor/Inc/Week5/Week5.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeWeek5_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x3632CA60,
-				0xDCCFBF94,
+				0x92C1007F,
+				0x5D0B932A,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_Week5.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_Week5.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Week5(Z_Construct_UPackage__Script_Week5, TEXT("/Script/Week5"), Z_Registration_Info_UPackage__Script_Week5, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x3632CA60, 0xDCCFBF94));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Week5(Z_Construct_UPackage__Script_Week5, TEXT("/Script/Week5"), Z_Registration_Info_UPackage__Script_Week5, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x92C1007F, 0x5D0B932A));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
